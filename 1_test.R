d03ffea7f57f37b24752adef3c9b625a8efe3d5b
rhijmans <r.hijmans@gmail.com>
2023-11-08 06:26:27 +0100

    m

--- 1_test.R
+++ 1_test.R
@@ -1,9 +1,9 @@
 prj_path <- wow::init("test", "agworkflows")
 
 x <- 1:10
-set.seed(1)
-y <- runif(10)
-xy <- cbind(x, y)
+set.seed(333)
+y <- runif(10) * 3 + 1:10
+xy <- data.frame(cbind(x, y))
 
 f <- file.path(prj_path, "data/raw/input.rds")
 
--- 3_test.R
+++ 3_test.R
@@ -5,8 +5,9 @@ d <- readRDS(f)
 
 png(file.path(prj_path, "img/3_scatter.png"))
 
-plot(d[,1], d[,2], pch=20, col="red")
-lines(d[,1], d[,3], col="red", lwd=2)
+plot(d[,1], d[,2], pch=20, cex=1.5, col="red", xlab="x", ylab="y", 
+	las=1, xlim=c(0,11), ylim=c(0,11), main="Jambo")
+lines(d[,1], d[,3], col="blue", lwd=2)
 
 dev.off()
 
