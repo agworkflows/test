bd332cecd3ab572b222ce6ec3b28cb86ef9c6d41
rhijmans <r.hijmans@gmail.com>
2023-11-14 13:21:30 +0100

    fix error

--- 1_test.R
+++ 1_test.R
@@ -7,7 +7,7 @@ xy <- data.frame(cbind(x, y))
 
 rwa3 = geodata::gadm("RWA", 1, path=pars$geo_path)
 
-f <- file.path(pars$prj_path, "data/raw/input.rds")
+#f <- file.path(pars$prj_path, "data/raw/input.rds")
 
 
 saveRDS(xy, f)
