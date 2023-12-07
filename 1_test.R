7f1cbfd34582b8af9ad581510a14e11947b17a20
rhijmans <r.hijmans@gmail.com>
2023-12-07 12:37:00 -0800

    x

--- 1_test.R
+++ 1_test.R
@@ -5,10 +5,6 @@ set.seed(333)
 y <- runif(12) * 3 + 1:12
 xy <- data.frame(cbind(x, y))
 
-rwa3 = geodata::gadm("RWA", 1, path=pars$geo_path)
-
-#f <- file.path(pars$prj_path, "data/raw/input.rds")
-
-
+f <- file.path(pars$prj_path, "data/raw/input.rds")
 saveRDS(xy, f)
 
